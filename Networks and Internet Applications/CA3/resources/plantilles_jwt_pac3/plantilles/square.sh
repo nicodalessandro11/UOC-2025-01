1af3e3f47fb28f23ec77aab3aba6b99337f36b0d
nicodalessandro11 <nicodalessandro11@gmail.com>
2025-05-19 20:36:01 +0200

    2025-05-19 mac

--- Networks and Internet Applications/CA3/resources/plantilles_jwt_pac3/plantilles/square.sh
+++ Networks and Internet Applications/CA3/resources/plantilles_jwt_pac3/plantilles/square.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 TOKEN=$1
 
+curl -X GET http://labxarxes.techlab.uoc.edu:8095/dslab-api/xai/square/2 \
+  -H "Authorization: Bearer $TOKEN"
+
+
 
 
---
+++ Networks and Internet Applications/CA3/resources/plantilles_jwt_pac3/plantilles/token.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -X POST http://labxarxes.techlab.uoc.edu:8095/dslab-api/token \
+  -H "X-TenantID: prova" \
+  -u e107@uoc.edu:TfM2023,
+
